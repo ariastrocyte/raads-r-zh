bd932b7d04d24f0b5b63fc45ab6fe1f59502d8ad
jinyizhi <jinyizhi666@jinyizhideMacBook-Air.local>
2025-07-01 01:43:52 +0800

    fix: clean up redirects and public

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [
-        tailwindcss()
-    ]
+    base: './',
+    plugins: [tailwindcss()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                form: 'form.html',
+                resources: 'resources.html'
+            }
+        }
+    }
 })
+
